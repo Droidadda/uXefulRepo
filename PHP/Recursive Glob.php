9309e4fe03a913574784ef5ec294275522d5bd3b
Chris Blakley <greatblakes@gmail.com>
2017-01-14 12:32:25 -0500

    Create Recursive Glob.php

---
+++ PHP/Recursive Glob.php
@@ -0,0 +1,11 @@
+//Recursive Glob
+function glob_r($pattern, $flags = 0){
+    $override = apply_filters('pre_glob_r', false, $pattern, $flags);
+    if ( $override !== false ){return $override;}
+ 
+    $files = glob($pattern, $flags);
+    foreach ( glob(dirname($pattern) . '/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir ){
+        $files = array_merge($files, glob_r($dir . '/' . basename($pattern), $flags));
+    }
+    return $files;
+}
