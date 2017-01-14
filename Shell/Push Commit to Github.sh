e3cc752dbf27bfafe91667961572969aca9b6399
Chris Blakley <greatblakes@gmail.com>
2017-01-14 11:54:10 -0500

    Create Push Commit to Github.sh

---
+++ Shell/Push Commit to Github.sh
@@ -0,0 +1,9 @@
+cd ~/Desktop/Work\ In\ Progress/\[Resources\]/GitHub/Nebula/
+git pull origin master
+ 
+#Then replace files as needed
+ 
+git add -A
+git status
+git commit -m "Bugfixes and other descriptive info here..."
+git push origin master
