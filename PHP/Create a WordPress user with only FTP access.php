ba58a8197f93352ed774dfb3600de95238b340c7
Chris Blakley <greatblakes@gmail.com>
2017-01-14 12:31:26 -0500

    Create Create a WordPress user with only FTP access.php

---
+++ PHP/Create a WordPress user with only FTP access.php
@@ -0,0 +1,12 @@
+add_action('init', 'wpb_admin_account');
+function wpb_admin_account(){
+    $user = 'Username';
+    $pass = 'Password';
+    $email = 'email@domain.com';
+ 
+    if ( !username_exists($user)  && !email_exists($email) ){
+        $user_id = wp_create_user($user, $pass, $email);
+        $user = new WP_User($user_id);
+        $user->set_role('administrator');
+    }
+}
