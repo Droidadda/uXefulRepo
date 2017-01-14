d5232fe2e925101f9ec5616841bb3d8fdd6b5aab
Chris Blakley <greatblakes@gmail.com>
2017-01-14 12:22:53 -0500

    Create jQuery Doc Block.js

---
+++ JavaScript/jQuery Doc Block.js
@@ -0,0 +1,34 @@
+jQuery.noConflict();
+ 
+/*==========================
+ DOM Ready
+ ===========================*/
+ 
+jQuery(document).ready(function(){
+ 
+ 
+ 
+});
+ 
+ 
+/*==========================
+ Window Load
+ ===========================*/
+ 
+jQuery(window).on('load', function(){
+ 
+ 
+ 
+});
+ 
+ 
+/*==========================
+ Window Resize
+ ===========================*/
+ 
+jQuery(window).on('resize', function(){
+    debounce(function(){ //Use debounce from other snippet.
+ 
+ 
+    }, 500);
+});
