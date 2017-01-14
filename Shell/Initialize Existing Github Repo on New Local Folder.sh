55f31f214b1595b5a8b5adf4b58e099dafe52906
Chris Blakley <greatblakes@gmail.com>
2017-01-14 11:53:44 -0500

    Create Initialize Existing Github Repo on New Local Folder.sh

---
+++ Shell/Initialize Existing Github Repo on New Local Folder.sh
@@ -0,0 +1,8 @@
+cd ~/Desktop/Work\ In\ Progress/\[Resources\]/GitHub/Whatever/
+git init
+git remote add origin https://github.com/user/repo.git
+git remote -v
+git pull origin master
+ 
+#If you are getting permission denied errors, make sure the local directory is writeable!
+sudo chmod 777 ~/Desktop/Work\ in\ Progress/\[Resources\]/GitHub/Nebula/
