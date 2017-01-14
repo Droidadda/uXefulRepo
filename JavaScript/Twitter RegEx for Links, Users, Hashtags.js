a1e8d8933fa4108962042101e78266084cd46af4
Chris Blakley <greatblakes@gmail.com>
2017-01-14 12:28:04 -0500

    Create Twitter RegEx for Links, Users, Hashtags.js

---
+++ JavaScript/Twitter RegEx for Links, Users, Hashtags.js
@@ -0,0 +1,7 @@
+//Convert Twitter usernames, hashtags, and URLs to links.
+function tweetLinks(tweet){
+    var newString = tweet.replace(/(http(\S)*)/g, '<a href="' + "$1" + '" target="_blank">' + "$1" + '</a>'); //Links that begin with "http"
+    newString = newString.replace(/#(([a-zA-Z0-9_])*)/g, '<a href="https://twitter.com/hashtag/' + "$1" + '" target="_blank">#' + "$1" + '</a>'); //Link hashtags
+    newString = newString.replace(/@(([a-zA-Z0-9_])*)/g, '<a href="https://twitter.com/' + "$1" + '" target="_blank">@' + "$1" + '</a>'); //Link @username mentions
+    return newString;
+}
