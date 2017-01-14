0923ae8fe78abdd2e293db2c08eadb51cacd5b71
Chris Blakley <greatblakes@gmail.com>
2017-01-14 12:32:11 -0500

    Create Recursive Array Search.php

---
+++ PHP/Recursive Array Search.php
@@ -0,0 +1,12 @@
+//Traverse multidimensional arrays
+function in_array_r($needle, $haystack, $strict = true){
+    $override = apply_filters('pre_in_array_r', false, $needle, $haystack, $strict);
+    if ( $override !== false ){return $override;}
+ 
+    foreach ($haystack as $item){
+        if ( ($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict)) ){
+            return true;
+        }
+    }
+    return false;
+}
