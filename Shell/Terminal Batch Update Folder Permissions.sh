0c442a224f3e1a80a466d10caec58f7375e657e2
Chris Blakley <greatblakes@gmail.com>
2017-02-01 14:49:53 -0500

    Create Terminal Batch Update Folder Permissions.sh

---
+++ Shell/Terminal Batch Update Folder Permissions.sh
@@ -0,0 +1,5 @@
+#Navigate to the directory that contains the folders to change permissions for.
+cd ~/Desktop/Work\ In\ Progress/
+
+#Change permissions recursively for all folders in this directory.
+sudo chmod -R 707 *
