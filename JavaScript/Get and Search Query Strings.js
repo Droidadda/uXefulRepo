b86051b207ead42720524f2da43ef00334b369c9
Chris Blakley <greatblakes@gmail.com>
2018-01-22 00:19:12 -0500

    Update Get and Search Query Strings.js

--- JavaScript/Get and Search Query Strings.js
+++ JavaScript/Get and Search Query Strings.js
@@ -1,28 +1,36 @@
 //Check Nebula for updated version of this...
 
 //Get query string parameters
-function getQueryStrings(){
-    queries = {};
-    var q = document.URL.split('?')[1];
-    if ( q ){
-        q = q.split('&');
-        for ( var i = 0; i < q.length; i++ ){
-            hash = q[i].split('=');
-            if ( hash[1] ){
-                queries[hash[0]] = hash[1];
-            } else {
-                queries[hash[0]] = true;
-            }
-        }
-    }
+function getQueryStrings(url){
+	if ( !url ){
+		url = document.URL;
+	}
+
+	var queries = {};
+	var queryString = url.split('?')[1];
+
+	if ( queryString ){
+		queryStrings = queryString.split('&');
+		for ( var i = 0; i < queryStrings.length; i++ ){
+			hash = queryStrings[i].split('=');
+			if ( hash[1] ){
+				 queries[hash[0]] = hash[1];
+			} else {
+				 queries[hash[0]] = true;
+			}
+		}
+	}
+
+	return queries;
 }
  
 //Search query strings for the passed parameter
-function get(query){
-    if ( !query ){
-        return queries;
-    } else {
-        return queries[query];
-    }
-    return false;
+function get(parameter, url){
+	var queries = getQueryStrings(url);
+
+	if ( !parameter ){
+		return queries;
+	}
+
+	return queries[parameter] || false;
 }
