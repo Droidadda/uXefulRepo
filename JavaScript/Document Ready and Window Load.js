45b2e28f86b38effbac3f8ec14fdfdbe9990f346
Chris Blakley <greatblakes@gmail.com>
2018-01-22 00:08:42 -0500

    Create Document Ready and Window Load.js

---
+++ JavaScript/Document Ready and Window Load.js
@@ -0,0 +1,25 @@
+//DOM Ready:
+
+//Using jQuery:
+jQuery(function(){
+  //do stuff
+});
+
+//Vanilla JS:
+document.addEventListener('DOMContentLoaded', function(event){
+  //do stuff
+});
+
+
+
+
+//Window Load:
+
+//Using jQuery
+jQuery(window).on('load', function(){
+  //do stuff
+});
+
+window.addEventListener('load', function() {
+  //do stuff
+})
